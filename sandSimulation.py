79e3db25a0fa97f50fa1d4451fbec719fb205141
T.P.R. van der Knaap <s2752077@fwn-bborg-edu-74-19.fmnsedu.rug.nl>
2018-09-12 14:13:41 +0200

    added spring objects between points

--- sandSimulation.py
+++ sandSimulation.py
@@ -3,14 +3,25 @@ import matplotlib.pyplot as plt
 #Currently points[0][0] is the left top element
 #So it has max y and min x
 
-class point:
+class Point:
 	def __init__(self, position):
-		self.neighbours = list()
+		self.springs = list()
 		self.position = position
 
-	def add_neighbour(self, neighbour):
-		self.neighbours.append(neighbour)
-		
+	def add_spring(self, spring):
+		print("add springieee")
+		self.springs.append(spring)
+
+
+class Spring:
+	def __init__(self, springconstant, breakforce, point1, point2):
+		self.springconstant = springconstant
+		self.breakforce = breakforce
+		self.point1 = point1
+		point1.add_spring(self)
+		self.point2 = point2
+		point2.add_spring(self)
+
 
 def calc_position(h, w, height, width):
 	field_size = 1.0
@@ -27,7 +38,7 @@ def create_points(height, width):
 	for h in range(0, height):
 		points.append(list())
 		for w in range(0, width):
-			points[h].append(point(calc_position(h, w, height, width)))
+			points[h].append(Point(calc_position(h, w, height, width)))
 	return points
 
 
@@ -35,19 +46,20 @@ def out_bounds(h, w, height, width):
 	return h < 0 or w < 0 or h >= height or w >= width 
 
 
-def add_neighbour_with_check(h, w, height, width, point, points):
+def create_spring_with_check(h, w, height, width, point, points):
+	springconstant = 1.0
+	breakforce = 1.0
 	if not out_bounds(h, w, height, width):
-		point.add_neighbour(points[h][w])
+		print("create spring")
+		Spring(springconstant, breakforce, point, points[h][w])
 
 
 def link_points(height, width, points):
 	for h in range(0, height):
 		for w in range(0, width):
 			point = points[h][w]
-			add_neighbour_with_check(h-1, w, height, width, point, points)
-			add_neighbour_with_check(h+1, w, height, width, point, points)
-			add_neighbour_with_check(h, w-1, height, width, point, points)
-			add_neighbour_with_check(h, w+1, height, width, point, points)
+			create_spring_with_check(h+1, w, height, width, point, points)
+			create_spring_with_check(h, w+1, height, width, point, points)
 
 
 def create_sqaure_point_grid():
@@ -69,31 +81,33 @@ def get_xs_ys(points):
 	return xs, ys
 
 
-def plot_lines_point(point):
-	for neighbour in point.neighbours:
-		point_y, point_x = point.position
-		neigh_y, neigh_x = neighbour.position 
-		if point_y >= neigh_y and point_x <= neigh_x:
-			plt.plot([point_x, neigh_x], [point_y, neigh_y], color='k', linestyle='-', zorder=1)
+def plot_springs_point(point):
+	print("plottt")
+	for spring in point.springs:
+		print("spring")
+		point1_y, point1_x = spring.point1.position
+		point2_y, point2_x = spring.point2.position 
+		if point1_y >= point2_y and point1_x <= point2_x:
+			plt.plot([point1_x, point2_x], [point1_y, point2_y], color='k', linestyle='-', zorder=1)
 
 
-def plot_lines(points):
+def plot_springs(points):
 	for row in points:
 		for point in row:
-			plot_lines_point(point)
+			plot_springs_point(point)
 
 
 def plot_points(points):
 	xs, ys = get_xs_ys(points)
 	plt.scatter(xs, ys, color="r", zorder=2)
-	plot_lines(points)
+	plot_springs(points)
 	plt.show()
 
 
-springconstant = 1.0
-friction_threshold = 0.9
-number of relaxation after is spring is broken = 20
-breaking treshold is gaussian distributed.
+#springconstant = 1.0
+#friction_threshold = 0.9
+#number of relaxation after is spring is broken = 20
+#breaking treshold is gaussian distributed.
 
 
 def main():
